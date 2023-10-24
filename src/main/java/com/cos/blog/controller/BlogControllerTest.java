5f6cb99c88a9cee3f3721dd281a91d66f00390c2
강승호 <kkang4913@naver.com>
2023-10-24 15:35:21 +0900

    환경세팅 완료

---
+++ src/main/java/com/cos/blog/controller/BlogControllerTest.java
@@ -0,0 +1,13 @@
+package com.cos.blog.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BlogControllerTest {
+
+    @GetMapping("/test/hello")
+    public String hello(){
+        return "<h1>Hello Spring Boot</h1><br><h2>2023.10.19일</h2>";
+    }
+}
